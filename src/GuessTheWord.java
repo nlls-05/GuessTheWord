3cab8d46418ebc3b6834ce5e5f4202f60fef79ad
Noureldin <your email>
2023-11-21 14:33:57 +0600

    rewrited the start again

--- src/GuessTheWord.java
+++ src/GuessTheWord.java
@@ -1,29 +1,23 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
 import java.util.Scanner;
+import java.util.Random;
 
 public class GuessTheWord {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        List<String> words = new ArrayList<>();
-        words.add("java");
-        words.add("computer");
         Random random = new Random();
-        String secretWord = words.get(random.nextInt(words.size()));
+
+        String[] words = {"java", "programming", "chocolate"};
+        String secretWord = words[random.nextInt(words.length)];
+
         char[] guessedWord = new char[secretWord.length()];
-        String[] hints = new String[secretWord.length()];
-        List<Character> correctLetters = new ArrayList<>();
-        List<Character> incorrectLetters = new ArrayList<>();
-        for (int i = 0; i < secretWord.length(); i++) {
+        for (int i = 0; i < guessedWord.length; i++) {
             guessedWord[i] = '_';
         }
-        int currentPlayer = 1;
+
         int attempts = 0;
         boolean wordGuessed = false;
 
-        System.out.println("Welcome to Guess the Word Game !");
-        System.out.println("Player 1, try to guess the word.");
-
+        System.out.println("Welcome to Guess the Word Game!");
+        System.out.println("Try to guess the word.");
     }
 }
