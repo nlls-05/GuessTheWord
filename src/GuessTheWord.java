d0349d3c56d8de395d9787b01ea05ede924d5846
Noureldin <your email>
2023-11-21 12:03:27 +0600

    np so far

--- src/GuessTheWord.java
+++ src/GuessTheWord.java
@@ -6,5 +6,17 @@ import java.util.Scanner;
 public class GuessTheWord {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        List<String> words = new ArrayList<>();
+        words.add("java");
+        words.add("computer");
+        Random random = new Random();
+        String secretWord = words.get(random.nextInt(words.size()));
+        char[] guessedWord = new char[secretWord.length()];
+        String[] hints = new String[secretWord.length()];
+        List<Character> correctLetters = new ArrayList<>();
+        List<Character> incorrectLetters = new ArrayList<>();
+        for (int i = 0; i < secretWord.length(); i++) {
+            guessedWord[i] = '_';
+        }
     }
 }
