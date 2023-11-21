a7544ecaab390ccc303ca9e6d87350414ff28a4d
Noureldin <your email>
2023-11-21 15:20:16 +0600

    first method

--- src/GuessTheWord.java
+++ src/GuessTheWord.java
@@ -6,7 +6,7 @@ public class GuessTheWord {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
 
-        String[] words = {"java", "programming", "chocolate"};
+        String[] words = {"java", "programming", "chocolate", "games"};
         String secretWord = words[random.nextInt(words.length)];
 
         char[] guessedWord = new char[secretWord.length()];
@@ -19,5 +19,21 @@ public class GuessTheWord {
 
         System.out.println("Welcome to Guess the Word Game!");
         System.out.println("Try to guess the word.");
+
     }
+    private static void displayWord(char[] word){
+        System.out.print("Word: ");
+        for (char letter : word) {
+            if (letter == '_') {
+                System.out.print("_ ");
+            } else {
+                System.out.print(Character.toUpperCase(letter) + " ");
+            }
+        }
+        System.out.println();
+    }
+
+
+
+
 }
